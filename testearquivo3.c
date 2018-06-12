8c400ddba61bb58acffeb858e93822ed037d3123
n-psilva <natanp.silva@hotmail.com>
2018-06-12 01:53:56 -0300

    Add files via upload

--- testearquivo3.c
+++ testearquivo3.c
@@ -1,28 +1,87 @@
 #include <stdio.h>
 
+
+
 int main()
+
 {
+
 	FILE *arq1=fopen("niteroi.txt","r");
+
 	FILE *arq2=fopen("saopaulo.txt","r");
+
 	FILE *arqgeral=fopen("geral.txt","w");
-	
-	char linha[100];
-	
-	while (fgets(linha,sizeof(linha),arq1))
-	{
-		fprintf(arqgeral,"%s", linha);
-	}
-	
-	while (fgets(linha,sizeof(linha),arq2))
-	{
-		fprintf(arqgeral,"%s", linha);
-	}
-	
-	fclose(arq1);
-	fclose(arq2);
-	fclose(arqgeral);
 
 	
+
+	char linha[100];
+
+	char linha2[100];
+
+	int x=0;
+	int i;
+
+	fgets(linha,sizeof(linha),arq1);
+
+	fgets(linha,sizeof(linha),arq1);
+
+	fgets(linha2,sizeof(linha2),arq2);
+
+	fgets(linha2,sizeof(linha2),arq2);
+
+    do
+
+	{
+
+		
+
+		fgets(linha2,sizeof(linha2),arq2);
+
 	
+       if(fgets(linha, 100, arq1)!=NULL)
+       {
+       	if(strcmp(linha,linha2) == -1)
+
+		fprintf(arqgeral,"%s\n", linha);
+        
+        else 
+		fprintf(arqgeral,"%s", linha2);
+	
+	   }
+	   if(!feof(arq2))
+	   {
+	   	if(strcmp(linha2,linha) == 1)
+
+		fprintf(arqgeral,"%s", linha2);
+		else
+		fprintf(arqgeral,"%s", linha2);
+	   }
+	 
+		
+
+	    
+
+	}while(!feof(arq1) || feof(arq2));
+    
+	
+	
+
+	
+
+
+
+	fclose(arq1);
+
+	fclose(arq2);
+
+	fclose(arqgeral);
+
+
+
+	
+
+	
+
 	return 0;
+
 }

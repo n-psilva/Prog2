4b2c2605c945076f028f7807eb6273c5a047ddeb
n-psilva <natanp.silva@hotmail.com>
2018-06-12 19:29:34 -0300

    Add files via upload

---
+++ ttt.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+	FILE *arq1=fopen("niteroi.txt","r");
+
+	FILE *arq2=fopen("saopaulo.txt","r");
+
+	FILE *arqgeral=fopen("geral.txt","w");
+
+
+	char linha[100];
+
+	char linha2[100];
+
+	int x=0;
+	int fimarq2;
+
+	fgets(linha,sizeof(linha),arq1);
+
+	fgets(linha,sizeof(linha),arq1);
+
+	fgets(linha2,sizeof(linha2),arq2);
+
+	fgets(linha2,sizeof(linha2),arq2);
+
+    do
+
+	{
+		
+        fimarq2=fgets(linha2,sizeof(linha2),arq2);
+	
+       if(fgets(linha, 100, arq1)!=NULL)
+       {
+       	if(strcmp(linha,linha2) == -1)
+       	{
+       		fprintf(arqgeral,"%s", linha);
+       	
+		}
+
+        
+        else 
+		fprintf(arqgeral,"%s", linha2);
+	
+	   }
+	   if(fimarq2!=NULL)
+	   {
+		fprintf(arqgeral,"%s", linha2);
+	   }
+	 
+		
+
+	}while(!feof(arq1) || !feof(arq2));
+	
+	
+	printf("Arquivo gerado!");
+    
+	
+
+	fclose(arq1);
+
+	fclose(arq2);
+
+	fclose(arqgeral);
+
+
+
+
+	return 0;
+
+}
